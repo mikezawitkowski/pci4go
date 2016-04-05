39729d0e0330e39771e4e78646de926d0dcf6850
mikezawitkowski <mike@mikezawitkowski.com>
2016-04-05 11:52:22 -0700

    Initial file commit (stub)

---
+++ clustering/feedvector.go
@@ -0,0 +1,18 @@
+/*
+
+feedvector.go
+
+This is similar to the python file described in Chapter 03,
+except that we will not be using the Feed Parser python module,
+and we may change a few things about this to be more universal
+in how it can be used.
+
+TODO: look at prior version of "generatefeedvector.py" from phlox or borg
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Finish this file!")
+}
